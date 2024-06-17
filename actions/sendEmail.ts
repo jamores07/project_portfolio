f6beedfb47545103a0c75f4d3d2fbbdd55ebdd34
John Amores <kayak.stated_0b@icloud.com>
2024-06-17 11:26:47 -0400

    Initial start

---
+++ actions/sendEmail.ts
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Resend } from 'resend';
+
+
+const sendEmail = () => {
+    const senderEmail = 'sender@email.com';
+    const message = 'Send message';
+
+    //validation
+    {/* To do: add validation */}
+
+    {/* Send email */}
+    
+}
+
+export default sendEmail
