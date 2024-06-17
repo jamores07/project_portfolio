737060cb11ab6d319ff1a123452e831d8c580919
John Amores <kayak.stated_0b@icloud.com>
2024-06-17 18:34:04 -0400

    Bug fixing

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,16 +1,47 @@
-import React from 'react';
-import { Resend } from 'resend';
+"use server";
 
+import React from "react";
+import { Resend } from "resend";
+import { validateString, getErrorMessage } from "@/lib/utils";
+import ContactFormEmail from "@/email/contact-form-email";
 
-const sendEmail = () => {
-    const senderEmail = 'sender@email.com';
-    const message = 'Send message';
+const resend = new Resend(process.env.RESEND_API_KEY);
 
-    //validation
-    {/* To do: add validation */}
+export const sendEmail = async (formData: FormData) => {
+  const senderEmail = formData.get("senderEmail");
+  const message = formData.get("message");
 
-    {/* Send email */}
-    
-}
+  // simple server-side validation
+  if (!validateString(senderEmail, 500)) {
+    return {
+      error: "Invalid sender email",
+    };
+  }
+  if (!validateString(message, 5000)) {
+    return {
+      error: "Invalid message",
+    };
+  }
 
-export default sendEmail
+  let data;
+  try {
+    data = await resend.emails.send({
+      from: "Contact Form <onboarding@resend.dev>",
+      to: "bytegrad@gmail.com",
+      subject: "Message from contact form",
+      reply_to: senderEmail,
+      react: React.createElement(ContactFormEmail, {
+        message: message,
+        senderEmail: senderEmail,
+      }),
+    });
+  } catch (error: unknown) {
+    return {
+      error: getErrorMessage(error),
+    };
+  }
+
+  return {
+    data,
+  };
+};
