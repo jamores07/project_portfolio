dc9a92a304f79a5a5d6e7cabab6fe56a88db6793
John Amores <kayak.stated_0b@icloud.com>
2024-06-17 18:57:23 -0400

    Bug fix -- experience section

--- components/experience.tsx
+++ components/experience.tsx
@@ -18,10 +18,10 @@ export default function Experience() {
   return (
     <section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
       <SectionHeading>My experience</SectionHeading>
-      <VerticalTimeline lineColor="">
+      <VerticalTimeline lineColor="" animate={false}>
         {experiencesData.map((item, index) => (
           <React.Fragment key={index}>
-            <VerticalTimelineElement
+            <VerticalTimelineElement 
               contentStyle={{
                 background:
                   theme === "light" ? "#f3f4f6" : "rgba(255, 255, 255, 0.05)",
